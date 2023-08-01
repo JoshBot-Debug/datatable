af0b940d45f02cef67706c9bef003265f3510611
JoshBot-Debug <joshuamyerswebdev@gmail.com>
2023-08-01 22:22:11 +0530

    fixed some css issues, added new autowidth feature and width

--- build/index.d.ts
+++ build/index.d.ts
@@ -2,7 +2,7 @@ import * as react_jsx_runtime from 'react/jsx-runtime';
 import styleInject from '/home/josh/Projects/@jjmyers/datatable/node_modules/style-inject/dist/style-inject.es.js';
 import * as react from 'react';
 
-var css_248z = ".myers-datatable .table-container {\n  display: table;\n  border-collapse: collapse;\n}\n.myers-datatable .table-row {\n  display: table-row;\n  position: relative;\n}\n.myers-datatable .table-cell {\n  display: table-cell;\n}\n.myers-datatable .table-header {\n  z-index: 1;\n}\n.myers-datatable .spinner {\n  border: 4px solid #f3f3f3; /* Light grey */\n  border-top: 4px solid #7e7e7e; /* Blue */\n  border-radius: 50%;\n  width: 15px;\n  height: 15px;\n  animation: spin 1s linear infinite;\n  margin-left: 5px;\n}\n.myers-datatable .spinner-loading-text {\n  margin-left: 10px;\n}\n@keyframes spin {\n  0% {\n    transform: rotate(0deg);\n  }\n  100% {\n    transform: rotate(360deg);\n  }\n}\n.myers-datatable .spinner-container {\n  position: absolute;\n  background-color: white;\n  top: 0;\n  left: 0;\n  height: 100%;\n  width: 100%;\n  display: flex;\n  align-items: center;\n}";
+var css_248z = ".myers-datatable .table-container {\n  display: table;\n}\n.myers-datatable .table-row {\n  display: table-row;\n  position: relative;\n}\n.myers-datatable .table-cell {\n  display: table-cell;\n}\n.myers-datatable .table-header-row {\n  z-index: 1;\n}\n.myers-datatable .spinner {\n  border: 4px solid #f3f3f3; /* Light grey */\n  border-top: 4px solid #7e7e7e; /* Blue */\n  border-radius: 50%;\n  width: 15px;\n  height: 15px;\n  animation: spin 1s linear infinite;\n  margin-left: 5px;\n}\n.myers-datatable .spinner-loading-text {\n  margin-left: 10px;\n}\n@keyframes spin {\n  0% {\n    transform: rotate(0deg);\n  }\n  100% {\n    transform: rotate(360deg);\n  }\n}\n.myers-datatable .spinner-container {\n  position: absolute;\n  background-color: white;\n  top: 0;\n  left: 0;\n  height: 100%;\n  width: 100%;\n  display: flex;\n  align-items: center;\n}";
 styleInject(css_248z);
 
 declare namespace Datatable {
@@ -24,6 +24,7 @@ declare namespace Datatable {
     renderCell?: (value: any, column: Column<Data>, row: Data) => React.ReactNode;
     setOptions?: string[];
     multiFilter?: boolean;
+    width?: number;
   } & ({
     datatype: Include<Datatype, "string" | "link" | "email" | "phone" | "name" | "paragraph" | "image">;
     filterOperations?: UseOperationFilter.TextFilterOperations[];
@@ -83,6 +84,8 @@ declare namespace Datatable {
     NoData?: React.ReactNode;
     onRowClick?: (row: Data, e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
     showOptionsOnRowClick?: boolean;
+    autoWidth: Record<keyof Data, { hasAutoSize: boolean, value: boolean }>;
+    toggleAutoWidth: (autoWidth?: boolean, field?: string) => void
   }
 
   interface Filter<Data extends Record<string, any>> {
@@ -97,6 +100,7 @@ declare namespace Datatable {
     column: Column<Data>;
     onClick?: (column: Column<Data>) => void;
     className?: string;
+    autoWidth?: Record<keyof Data, { hasAutoSize: boolean, value: boolean }>;
   }
 
   type DatatableFilterProps<Operation> = { multiFilter?: boolean; setOptions?: string[]; datatype: string; field: string; filterOperations?: Operation[] };
@@ -120,6 +124,8 @@ declare namespace Datatable {
     NoData?: React.ReactNode;
     onRowClick?: (row: Data, e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
     showOptionsOnRowClick?: boolean;
+    autoWidth?: Record<keyof Data, { hasAutoSize: boolean, value: boolean }>;
+    toggleAutoWidth: (autoWidth?: boolean, field?: string) => void;
   }
 
 
@@ -310,6 +316,11 @@ declare function useDatatable<Data extends Record<string, any>>(config: Datatabl
     selectable: Datatable.UseSelectable.HookReturn;
     setFilter: Datatable.UseSetFilter.HookReturn<Data>;
     operationFilter: Datatable.UseOperationFilter.HookReturn<Data, string>;
+    autoWidth: Record<keyof Data, {
+        hasAutoSize: boolean;
+        value: boolean;
+    }>;
+    toggleAutoWidth: (autoWidth?: boolean, field?: string) => void;
     updateFilter: react.Dispatch<react.SetStateAction<Datatable.Filter<Data>>>;
     Datatable: typeof RichDatatable;
 };
